977d9fb6a95c449cd5f972da4998865a26e8cce4
leoz <leoz@leozs-MacBook-Pro.local>
2023-11-20 10:39:07 +0700

    update components

--- src/app/music-search/page.tsx
+++ src/app/music-search/page.tsx
@@ -1,5 +1,6 @@
 import { SearchIcon, PlusCircleIcon } from "lucide-react";
 
+
 export default function MusicSearchPage() {
     return <div className="flex flex-col h-full gap-3">
         <div className="flex relative w-full content-center text-center items-center gap-4">
@@ -8,7 +9,7 @@ export default function MusicSearchPage() {
         </div>
 
         <div className="flex flex-wrap overflow-y-scroll">
-            {Array(50).fill(10).map((_, idx) => <div key={`result_${idx}`} className="flex flex-col w-1/2 @lg:w-1/3 @2xl:w-1/4 @5xl:w-1/5 gap-2 p-5 hover:bg-slate-700/50 rounded-lg">
+            {Array(20).fill(10).map((_, idx) => <div key={`result_${idx}`} className="flex flex-col w-1/2 @lg:w-1/3 @2xl:w-1/4 @5xl:w-1/5 gap-2 p-5 hover:bg-slate-700/50 rounded-lg">
                 <div className="flex bg-red-500 w-full aspect-w-1 aspect-h-1 rounded-xl"></div>
                 <div className="flex flex-col gap-1 text-sm">
                     This is Taylor Swift
